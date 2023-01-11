f39fb6af79b078ba3dba47bb5156d88a2f5fa281
MintarasGrinius <74651345+MintarasGrinius@users.noreply.github.com>
2023-01-11 20:19:44 +0200

    Buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,7 +56,7 @@
 			<button
 				type="submit"
 				class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"
-				>Button</button
+				>Login</button
 			>
 			<a class="text-xs mt-3 hover:no-underline" href="/register">Register</a>
 		</form>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -113,7 +113,7 @@
 			<button
 				type="submit"
 				class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"
-				>Button</button
+				>Register</button
 			>
 			<a class="text-xs mt-3 hover:no-underline" href="/">Login</a>
 		</form>
